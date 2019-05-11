0c58b6a420495db1f0ce81bc904e097d1206a57c
Navpreet Singh <navpreet.oberoi@shuttl.com>
2019-05-11 12:48:31 +0530

    Adding parametrised test via DataProvider
    Refactoring gitignore file

---
+++ src/test/java/ZipCodeApi_GetRequestTests/MultipleLocationsViaParametrisation.java
@@ -0,0 +1,51 @@
+package ZipCodeApi_GetRequestTests;
+
+import Utility.RestUtil;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * @author navpreetsingh on 11/05/19
+ * @project RestApiAutomation
+ */
+public class MultipleLocationsViaParametrisation extends RestUtil {
+
+    String baseURI = getValueFromPropertyFile("ZIPPO_API_BASEURL");
+
+
+    @DataProvider(name = "data-provider")
+    public static Object[][] zipCodesAndPlaces() {
+        return new Object[][]{
+                {"in", "110058", "Janakpuri"},
+                {"us", "12345", "Schenectady"},
+                {"ca", "B2R", "Waverley"}
+        };
+    }
+
+    @BeforeTest
+    public void setup() {
+        setBaseURI(baseURI);
+    }
+
+
+    @Test(dataProvider = "data-provider")
+    public void requestZipCodesFromCollection_checkPlaceNameInResponseBody_expectSpecifiedPlaceName(String countryCode, String zipCode, String expectedPlaceName) {
+
+        given()
+                .log().all()
+                .pathParam("countryCode", countryCode).pathParam("zipCode", zipCode).
+                when()
+                .get("http://zippopotam.us/{countryCode}/{zipCode}").
+                then()
+                .log().all()
+                .assertThat()
+                .body("places[0].'place name'", equalTo(expectedPlaceName));
+    }
+
+
+}
+
